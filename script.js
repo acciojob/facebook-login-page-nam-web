911bd1ea224f810363232f9e78932f7883e1a139
nam-web <65065658+nam-web@users.noreply.github.com>
2024-06-11 11:53:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your JS code here. If required.
+cy.get('#signup').click(); // This will simulate a click on the element with id 'signup'
+cy.url().should('include', '/reg/'); // This checks if the current URL includes '/reg/'
